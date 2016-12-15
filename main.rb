88918ce73d82b83e40f0228e86ac2a130e397ba4
Abigail Boyton <boytonae@aston.ac.uk>
2016-12-15 01:43:38 +0000

    Add main to generate and save results

--- main.rb
+++ main.rb
@@ -1,15 +1,41 @@
 require_relative 'particle_swarm'
+require_relative 'evolutionary'
+require_relative 'random_search'
 
-# training_data = SearchSpace.new('cwk_train.csv')
+n = 100
+loops = 1000
 
-swarm = ParticleSwarm.new
+random_results = open('Results\reults_random.csv', 'w')
 
-print "\n****************************\n"
-print swarm.global_best_cost
+ (0...n).each do
+  random = RandomSearch.new
 
-(0..1000).each do
-  swarm.search
+  (0...loops).each do
+    random.iterate
+  end
+  random_results.write("#{random.global_best_cost}, #{random.global_best} \n")
 end
-print "\n****************************\n"
-print swarm.global_best_cost
-print "\n****************************\n"
+random_results.close
+
+pso_results = open('Results\reults_pso.csv', 'w')
+
+(0...n).each do
+  swarm = ParticleSwarm.new
+  (0...loops).each do
+    swarm.search
+  end
+  pso_results.write("#{swarm.global_best_cost}, #{swarm.global_best} \n")
+end
+pso_results.close
+
+evolve_results = open('Results\results_evolve.csv', 'w')
+
+(0...n).each do
+  evolve = Evolutionary.new(100)
+  (0...loops).each do
+    evolve.iterate
+  end
+  evolve_results.write("#{evolve.global_best_cost}, #{evolve.global_best} \n")
+end
+evolve_results.close
+
