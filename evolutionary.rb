f43c2d9a665bf741150afb96e7beb1133bb9dadd
Abigail Boyton <boytonae@aston.ac.uk>
2016-12-15 01:41:55 +0000

    Add iterate to evolution and tidy up

--- evolutionary.rb
+++ evolutionary.rb
@@ -1,8 +1,5 @@
-# get a random population of weigths
-# select candidates (tournament? other? roulette)
-# mutate/crossover candidates until you have a new popiulation
 class Evolutionary
-  attr_reader :population
+  attr_reader :population, :global_best
   def initialize(pop_size)
     @training_data = SearchSpace.new('cwk_train.csv')
 
@@ -10,10 +7,38 @@ class Evolutionary
     (0...pop_size).each do
       @population << @training_data.generate_valid_weights
     end
+
+    @global_best = current_best
   end
 
   def iterate
+    filler_per_gen = 10
 
+    if filler_per_gen > population.length
+      raise 'More filler than the population size!'
+    end
+
+    # select the new parents
+    parents = tournament_selection(3, population.length / 2 )
+
+    #breed them to fill up most of the next generation
+    next_gen = []
+    (0...(population.length - filler_per_gen)).each do
+      next_gen << breed(parents)
+    end
+
+    #add random filler to keep the 'gene pool' fresh
+    (0..filler_per_gen).each do
+      next_gen << @training_data.generate_valid_weights
+    end
+
+    @population = next_gen
+
+    @global_best = current_best if current_best_cost < global_best_cost
+  end
+
+  def global_best_cost
+    @training_data.evaluate(global_best)
   end
 
   private
@@ -25,30 +50,51 @@ class Evolutionary
 
     for i in (0...parents[0].length) do
       random_parent = ((0...parents.length).to_a).sample
-      puts random_parent
+
       child << parents[random_parent][i]
     end
-    print child
     child
   end
 
-  def rand_selection(num_candidates, population)
+  def rand_selection(num_candidates, pool=population)
     candidates = []
 
     (0...num_candidates).each do
-      rand_candidate = ((0...population.length).to_a).sample
+      rand_candidate = ((0...pool.length).to_a).sample
 
-      candidates << population[rand_candidate]
+      candidates << pool[rand_candidate]
     end
 
     candidates
   end
 
-  def best_selection(num_candidates, population)
-    sorted_pop = population.sort_by do |member|
+  def best_selection(num_candidates, pool=population)
+    sorted_pop = pool.sort_by do |member|
       @training_data.evaluate(member)
     end
 
     sorted_pop[0...num_candidates]
+  end
+
+  def tournament_selection(num_candidates, tournament_size)
+    if num_candidates > tournament_size
+      raise 'Tournament too small!'
+    end
+
+    tournament = rand_selection(tournament_size)
+
+    best_selection(num_candidates, tournament)
+  end
+
+  def current_best
+    sorted_pop = population.sort_by do |member|
+      @training_data.evaluate(member)
+    end
+
+    sorted_pop[0]
+  end
+
+  def current_best_cost
+    @training_data.evaluate(current_best)
   end
 end
